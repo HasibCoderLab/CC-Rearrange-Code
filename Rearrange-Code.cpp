0af802eb473f7ad68e80bcfb6092cc3dda05a64e
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-27 21:25:54 +0600

    solution Done

---
+++ Rearrange-Code.cpp
@@ -0,0 +1,19 @@
+// ================================================
+//                Platform : CodeChef
+//                Contest  : Practice (Beginner)
+//                Problem  : Rearrange Code
+//                Language : C++
+//                Topic    : [Rearrange Code]
+//                File     : Rearrange Code.cpp
+// ================================================
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a << b;
+    int sum = a +b ;
+    cout<<sum;
+    return 0;
+}
